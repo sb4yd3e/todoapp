483c4a01a427031c5b0f1c61cf49984e681994d1
JJSD <jedsadang@orisma.com>
2016-05-27 20:48:04 +0700

    remove config.js bundle.js

--- src/config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  port: 5000,
-  processName: 'TodoApp',
-  rootURL: 'http://localhost',
-  neo4jURL: '192.168.99.100:32769',
-  neo4jUSER: 'neo4j',
-  neo4jPASS:  '1234',
-  secret: 'adrubale'
-}
